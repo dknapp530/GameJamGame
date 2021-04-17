67fd64b34ec31b2b32a12cf377ae20ea69ee5d0f
David Knapp <dknapp530@yahoo.com>
2021-04-17 15:13:01 -0500

    Added enemy prefab and enemies now reprioratize targets based on distance

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private List<GameObject> enemyPool;
+    [SerializeField]
+    private int enemyPoolSize;
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private List<GameObject> targets;
+    void Start()
+    {
+        Target[] targetArray = FindObjectsOfType<Target>() as Target[];
+        
+        for(int i = 0; i < targetArray.Length; i++)
+        {
+            targets.Add(targetArray[i].gameObject);
+        }
+
+        enemyPool = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < enemyPoolSize; i++)
+        {
+            tmp = Instantiate(enemyPrefab);
+            tmp.SetActive(false);
+            enemyPool.Add(tmp);
+        }
+    }
+    void Update()
+    {
+        
+    }
+    public List<GameObject> GetTargets()
+    {
+        return targets;
+    }
+}
