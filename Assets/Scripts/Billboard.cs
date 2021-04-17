c83020b2e8d38eab33eb68ba87ec0ad02a784901
David Knapp <dknapp530@yahoo.com>
2021-04-17 13:36:36 -0500

    Added new billboard and saved the scene

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private Renderer renderer;
+    private void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if (renderer.isVisible)
+        {
+            Vector3 lookPosition = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
+            transform.LookAt(lookPosition, Vector3.up);
+        }
+    }
+}
