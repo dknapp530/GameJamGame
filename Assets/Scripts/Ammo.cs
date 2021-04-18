9cc89d2f8fd2f1710c0a848e5ad575d3c5eb9faa
Austin Matz <mrmatzz@gmail.com>
2021-04-17 19:10:52 -0500

    1
    1

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField]
+    private float ammo = 100.0f;
+    private readonly Weapon gunBoy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void RefillAmmo()
+    {
+
+    }
+    public bool CheckAmmo()
+    {
+        bool returnValue = false;
+        if(ammo > 0)
+        {
+            returnValue = true;
+            ammo--;
+        }
+        return returnValue;
+    }
+
+    public float GetAmmo()
+    {
+        return ammo;
+    }
+}
